0e10451bf2975d97b79602f6546f9125d0d2786e
Krystal Guzman <kryguzman@gmail.com>
2019-12-16 12:19:56 -0800

    makePersonObject done

--- index.js
+++ index.js
@@ -39,8 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+
+function makePersonObject(id, name, email) {
+  var object = {
+  id: id,
+  name: name,
+  email: email,
+  }
+  return object;
 }
 
 /**
